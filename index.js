a034dc76782bec6154533cf8d3dabd01ec2b2ef6
Rapids <crinionr@msu.edu>
2024-03-13 14:22:35 -0400

    cdn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createClient } from './node_modules/@supabase/supabase-js';
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://xyzcompany.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmhqa2ZpZ2ZxZWN2dmx3cmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNTA2NjAsImV4cCI6MjAyNTkyNjY2MH0.9nHfKjEByW-P31hRREUiVmr3ET6lHqTcYL_mxRfs-8U')
 

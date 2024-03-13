6028a1bc21d963718b0fb1cf18b2ec94a9dfef0a
Rapids <crinionr@msu.edu>
2024-03-13 13:56:57 -0400

    js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add JavaScript below
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://ruvhjkfigfqecvvlwrdp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmhqa2ZpZ2ZxZWN2dmx3cmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNTA2NjAsImV4cCI6MjAyNTkyNjY2MH0.9nHfKjEByW-P31hRREUiVmr3ET6lHqTcYL_mxRfs-8U'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innterHTML += '<li>${book.title}</li>';
+}

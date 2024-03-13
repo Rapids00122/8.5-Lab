b5bd9c18b60ddcaca7ec855f2204710001958923
Rapids <crinionr@msu.edu>
2024-03-13 14:11:59 -0400

    key

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ruvhjkfigfqecvvlwrdp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmhqa2ZpZ2ZxZWN2dmx3cmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNTA2NjAsImV4cCI6MjAyNTkyNjY2MH0.9nHfKjEByW-P31hRREUiVmr3ET6lHqTcYL_mxRfs-8U'
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xyzcompany.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmhqa2ZpZ2ZxZWN2dmx3cmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNTA2NjAsImV4cCI6MjAyNTkyNjY2MH0.9nHfKjEByW-P31hRREUiVmr3ET6lHqTcYL_mxRfs-8U')
+
 
 let { data: books, error } = await supabase
   .from('books')
@@ -9,5 +11,5 @@ let { data: books, error } = await supabase
 
 for (let book of books) {
     let bookList = document.getElementById('books');
-    bookList.innterHTML += '<li>${book.title}</li>';
+    bookList.innterHTML += `<li>${book.title}</li>`;
 }
